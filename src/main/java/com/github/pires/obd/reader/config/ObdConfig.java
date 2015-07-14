94f1ed2a75358c7b64a46ecb67362c62810d78cf
nomwerp <git@johannesbode.nl>
2015-07-14 19:50:09 +0200

    added new obd commands , fixes #113

--- src/main/java/com/github/pires/obd/reader/config/ObdConfig.java
+++ src/main/java/com/github/pires/obd/reader/config/ObdConfig.java
@@ -2,21 +2,27 @@ package com.github.pires.obd.reader.config;
 
 import com.github.pires.obd.commands.ObdCommand;
 import com.github.pires.obd.commands.SpeedObdCommand;
+import com.github.pires.obd.commands.control.CommandControlModuleVoltageObdCommand;
 import com.github.pires.obd.commands.control.CommandEquivRatioObdCommand;
+import com.github.pires.obd.commands.control.DistanceTraveledSinceCodesClearedObdCommand;
+import com.github.pires.obd.commands.control.DistanceTraveledWithMILOnObdCommand;
 import com.github.pires.obd.commands.control.DtcNumberObdCommand;
 import com.github.pires.obd.commands.control.TimingAdvanceObdCommand;
 import com.github.pires.obd.commands.control.TroubleCodesObdCommand;
+import com.github.pires.obd.commands.control.VinObdCommand;
 import com.github.pires.obd.commands.engine.EngineLoadObdCommand;
 import com.github.pires.obd.commands.engine.EngineRPMObdCommand;
 import com.github.pires.obd.commands.engine.EngineRuntimeObdCommand;
 import com.github.pires.obd.commands.engine.MassAirFlowObdCommand;
 import com.github.pires.obd.commands.engine.ThrottlePositionObdCommand;
 import com.github.pires.obd.commands.fuel.FindFuelTypeObdCommand;
+import com.github.pires.obd.commands.fuel.FuelConsumptionRateObdCommand;
 import com.github.pires.obd.commands.fuel.FuelEconomyObdCommand;
 import com.github.pires.obd.commands.fuel.FuelLevelObdCommand;
 import com.github.pires.obd.commands.fuel.FuelTrimObdCommand;
 import com.github.pires.obd.commands.pressure.BarometricPressureObdCommand;
 import com.github.pires.obd.commands.pressure.FuelPressureObdCommand;
+import com.github.pires.obd.commands.pressure.FuelRailPressureObdCommand;
 import com.github.pires.obd.commands.pressure.IntakeManifoldPressureObdCommand;
 import com.github.pires.obd.commands.temperature.AirIntakeTemperatureObdCommand;
 import com.github.pires.obd.commands.temperature.AmbientAirTemperatureObdCommand;
@@ -34,24 +40,30 @@ public final class ObdConfig {
     ArrayList<ObdCommand> cmds = new ArrayList<>();
 
     // Control
+    cmds.add(new CommandControlModuleVoltageObdCommand());
     cmds.add(new CommandEquivRatioObdCommand());
+    cmds.add(new DistanceTraveledSinceCodesClearedObdCommand());
+    cmds.add(new DistanceTraveledWithMILOnObdCommand());
     cmds.add(new DtcNumberObdCommand());
     cmds.add(new TimingAdvanceObdCommand());
     cmds.add(new TroubleCodesObdCommand());
+    cmds.add(new VinObdCommand());
 
     // Engine
     cmds.add(new EngineLoadObdCommand());
     cmds.add(new EngineRPMObdCommand());
     cmds.add(new EngineRuntimeObdCommand());
     cmds.add(new MassAirFlowObdCommand());
+    cmds.add(new ThrottlePositionObdCommand());
 
     // Fuel
+    cmds.add(new FindFuelTypeObdCommand());
+    cmds.add(new FuelConsumptionRateObdCommand());
     // cmds.add(new AverageFuelEconomyObdCommand());
     cmds.add(new FuelEconomyObdCommand());
+    cmds.add(new FuelLevelObdCommand());
     // cmds.add(new FuelEconomyMAPObdCommand());
     // cmds.add(new FuelEconomyCommandedMAPObdCommand());
-    cmds.add(new FindFuelTypeObdCommand());
-    cmds.add(new FuelLevelObdCommand());
     cmds.add(new FuelTrimObdCommand(FuelTrim.LONG_TERM_BANK_1));
     cmds.add(new FuelTrimObdCommand(FuelTrim.LONG_TERM_BANK_2));
     cmds.add(new FuelTrimObdCommand(FuelTrim.SHORT_TERM_BANK_1));
@@ -60,6 +72,7 @@ public final class ObdConfig {
     // Pressure
     cmds.add(new BarometricPressureObdCommand());
     cmds.add(new FuelPressureObdCommand());
+    cmds.add(new FuelRailPressureObdCommand());
     cmds.add(new IntakeManifoldPressureObdCommand());
 
     // Temperature
@@ -69,7 +82,7 @@ public final class ObdConfig {
 
     // Misc
     cmds.add(new SpeedObdCommand());
-    cmds.add(new ThrottlePositionObdCommand());
+
 
     return cmds;
   }
