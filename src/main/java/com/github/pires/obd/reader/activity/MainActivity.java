8bc8e80143c548dfb33facc06ba57db09f643989
jbits <szy.rob@gmail.com>
2016-06-03 14:17:36 -0400

    Don't show dialog to report issues if developer email is empty

--- src/main/java/com/github/pires/obd/reader/activity/MainActivity.java
+++ src/main/java/com/github/pires/obd/reader/activity/MainActivity.java
@@ -489,8 +489,8 @@ public class MainActivity extends RoboActivity implements ObdProgressListener, L
 
         releaseWakeLockIfHeld();
 
-        final String devemail = prefs.getString(ConfigActivity.DEV_EMAIL_KEY, null);
-        if (devemail != null) {
+        final String devemail = prefs.getString(ConfigActivity.DEV_EMAIL_KEY, null).trim();
+        if (devemail != null && !devemail.isEmpty()) {
             DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
